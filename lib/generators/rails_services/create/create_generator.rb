f59c6f43ce45dee3c20d6b1b88f1212d03d486f0
Craig Kaminsky <imageaid@gmail.com>
2014-07-26 14:18:13 -0600

    working #1
    cleaning up driectory management code when destroying a service and spec.

--- lib/generators/rails_services/create/create_generator.rb
+++ lib/generators/rails_services/create/create_generator.rb
@@ -7,23 +7,23 @@ module RailsServices
       argument :sub_folder, type: :string, default: ''
 
       def create_service
-        template 'base_service.rb.erb', "app/services/#{service_file_path}"
-        template 'base_service_spec.rb.erb', "spec/services/#{spec_file_path}"
+        template 'base_service.rb.erb', "app/services/#{service_directory}/#{service_file}"
+        template 'base_service_spec.rb.erb', "spec/services/#{service_directory}/#{spec_file}"
       end
 
       private
-      def service_file_path
-        file_and_path = "#{parent_name.underscore}/"
-        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
-        file_and_path += "#{service_name.underscore}.rb"
-        file_and_path
+      def service_directory
+        file_path = "#{parent_name.underscore}"
+        file_path += "/#{sub_folder.underscore}" if sub_folder.present?
+        file_path
       end
 
-      def spec_file_path
-        file_and_path = "#{parent_name.underscore}/"
-        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
-        file_and_path += "#{service_name.underscore}_spec.rb"
-        file_and_path
+      def service_file
+        "#{service_name.underscore}.rb"
+      end
+
+      def spec_file
+        "#{service_name.underscore}_spec.rb"
       end
 
       def qualified_name
--- lib/generators/rails_services/destroy/destroy_generator.rb
+++ lib/generators/rails_services/destroy/destroy_generator.rb
@@ -6,31 +6,48 @@ module RailsServices
       argument :sub_folder, type: :string, default: ''
 
       def destroy_service
-        remove_file "app/services/#{service_file_path}"
-        remove_file "spec/services/#{spec_file_path}"
+        remove_file "app/services/#{full_service_directory}/#{service_file}"
+        remove_file "spec/services/#{full_service_directory}/#{spec_file}"
+
+        manage_containing_directories('app', 'spec')
       end
 
       private
-      def service_file_path
-        file_and_path = "#{parent_name.underscore}/"
-        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
-        file_and_path += "#{service_name.underscore}.rb"
-        file_and_path
+      def full_service_directory
+        file_path = "#{service_parent_directory}"
+        file_path += "/#{sub_folder.underscore}" if sub_folder.present?
+        file_path
       end
 
-      def spec_file_path
-        file_and_path = "#{parent_name.underscore}/"
-        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
-        file_and_path += "#{service_name.underscore}_spec.rb"
-        file_and_path
+      def service_parent_directory
+        "#{parent_name.underscore}"
       end
 
-      def qualified_name
-        name = "#{parent_name.camelize}::"
-        name += "#{sub_folder.camelize}::" if sub_folder.present?
-        name += "#{service_name.camelize}"
-        name
+      def service_file
+        "#{service_name.underscore}.rb"
       end
+
+      def spec_file
+        "#{service_name.underscore}_spec.rb"
+      end
+
+      def manage_containing_directories(*directory_types)
+        remove_dir("app/services/#{full_service_directory}")
+        remove_dir("spec/services/#{full_service_directory}")
+        # directory_types.each do |directory_type|
+        #   if Dir["#{Rails.root}/#{directory_type}/services/#{full_service_directory}/*"].empty?
+        #     Dir.delete(Dir["#{Rails.root}/#{directory_type}/services/#{full_service_directory}"])
+        #     manage_parent_directory(directory_type) if sub_folder.present?
+        #   end
+        # end
+      end
+
+      def manage_parent_directory(directory_type)
+        if Dir["#{Rails.root}/#{directory_type}/services/#{service_parent_directory}/*"].empty?
+          Dir.delete(Dir["#{Rails.root}/#{directory_type}/services/#{service_parent_directory}"])
+        end
+      end
+
     end
   end
 end
