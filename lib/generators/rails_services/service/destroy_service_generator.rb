391fa4efcb109e96e186b5932e55c07a3a536836
Craig Kaminsky <imageaid@gmail.com>
2014-07-26 13:30:52 -0600

    working #1 - adding base class and methods for destroy.

---
+++ lib/generators/rails_services/service/destroy_service_generator.rb
@@ -0,0 +1,36 @@
+module RailsServices
+  module Generators
+    class DestroyServiceGenerator < Rails::Generators::Base
+      argument :service_name, type: :string
+      argument :parent_name, type: :string
+      argument :sub_folder, type: :string, default: ''
+
+      def destroy_service
+        remove_file "app/services/#{service_file_path}"
+        remove_file "spec/services/#{spec_file_path}"
+      end
+
+      private
+      def service_file_path
+        file_and_path = "#{parent_name.underscore}/"
+        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
+        file_and_path += "#{service_name.underscore}.rb"
+        file_and_path
+      end
+
+      def spec_file_path
+        file_and_path = "#{parent_name.underscore}/"
+        file_and_path += "#{sub_folder.underscore}/" if sub_folder.present?
+        file_and_path += "#{service_name.underscore}_spec.rb"
+        file_and_path
+      end
+
+      def qualified_name
+        name = "#{parent_name.camelize}::"
+        name += "#{sub_folder.camelize}::" if sub_folder.present?
+        name += "#{service_name.camelize}"
+        name
+      end
+    end
+  end
+end
